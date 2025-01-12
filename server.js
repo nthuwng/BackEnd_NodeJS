d501caf5d682d68493672dac23e02c4f22d7f300
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-01-12 23:21:52 +0700

    video 14

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
-//Load HTTP module (thư viện đã có sẵn khi cài đặt node.js)
-const http = require("http");
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = "localhost"; //cái này === http://localhost
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n hoi dan it voi Edric");
-});
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
-//tự động chạy trên localhost
-// server.listen(port, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
