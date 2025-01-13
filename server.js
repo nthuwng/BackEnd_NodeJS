78d5eb68b1f7a0f77f1cb84d4d5abdb009afc091
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-01-13 11:01:00 +0700

    video 17

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/abc', (req, res) => {
+  res.send('Check ABC')
+})
+
+app.get('/hoidanit', (req, res) => {
+  res.send('<h1>Check hoidanit</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
