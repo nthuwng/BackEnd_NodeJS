be97d7388ea6b70f60a964b1010b2bf874cab52c
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-01-14 15:03:30 +0700

    video 30

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express'); //commonjs
 const configViewEngine = require('./config/viewEngine'); //commonjs
 const webRoute = require('./routes/web'); //commonjs
+const mysql = require('mysql2');
+
 
 const app = express();// app express
 const port = process.env.PORT || 8888; //port => hardcode . uat .prod
@@ -12,6 +14,24 @@ configViewEngine(app);
 
 //khai báo route
 app.use('/', webRoute);
+
+//test connection mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'hoidanit',
+    port: 3309,
+    password: '123456'
+  });
+  
+
+  connection.query(
+    'SELECT * FROM Users',
+    function (err, results, fields) {
+      console.log("results= ",results); // results contains rows returned by server
+      console.log("fields= ",fields); // fields contains extra meta data about results, if available
+    }
+  );
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
