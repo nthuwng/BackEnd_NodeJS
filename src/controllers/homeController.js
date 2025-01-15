c29eb5937be344032b33afd2433bdc01ed75084a
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-01-15 16:38:01 +0700

    video 38

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,22 +1,37 @@
-const connection = require('../config/database'); //commonjs
+const connection = require("../config/database"); //commonjs
 
-const getHomePage = (req , res) => {
-    
-    return res.render('home.ejs')
-}
+const getHomePage = (req, res) => {
+  return res.render("home.ejs");
+};
 
 const getABC = (req, res) => {
-    res.send('check ABC')
-}
+  res.send("check ABC");
+};
 
 const getHoiDanIT = (req, res) => {
-    res.render('sample.ejs')
-}
+  res.render("sample.ejs");
+};
 
 const postCreateUser = (req, res) => {
-    console.log(">>> req.body: ", req.body)
-    res.send('create a new user')
-}
-module.exports = {  
-    getHomePage,getABC,getHoiDanIT,postCreateUser
-}
+  let email = req.body.email;
+  let name = req.body.myname;
+  let city = req.body.city;
+
+  console.log("email= ", email, "name= ", name, "city= ", city);
+
+  connection.query(
+    `INSERT INTO Users (email, name, city)
+        VALUES (?,?,?)`,
+    [email, name, city],
+    function (err, results) {
+      console.log(results);
+      res.send("Create user success");
+    }
+  );
+};
+module.exports = {
+  getHomePage,
+  getABC,
+  getHoiDanIT,
+  postCreateUser,
+};
