68eb16dd0d1972796f460aa13823caa549b1bf56
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-01-24 11:12:05 +0700

    video 76

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,9 +3,11 @@ const routerAPI = express.Router();
 const {
   getUsersAPI,
   postCreateUserAPI,
+  putUpdateUserAPI,
 } = require("../controllers/apiController");
 
 routerAPI.get("/user", getUsersAPI);
 routerAPI.post("/user", postCreateUserAPI);
+routerAPI.put("/user", putUpdateUserAPI);
 
 module.exports = routerAPI;
