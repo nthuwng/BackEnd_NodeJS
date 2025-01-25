2ee255c5ec405f28e2cb9d353e67b198ca6614d0
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-01-25 13:39:40 +0700

    video 81

---
+++ src/models/customer.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: String,
+    phone: String,
+    email: String,
+    image: String,
+    description: String,
+  },
+  { timestamps: true }
+);
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
